737c014c276e2eb0333516ebf3d181e3e8c5883e
FabienneDieZitrone <ttrella@mikropartner.de>
2025-07-10 12:39:40 +0200

    fix: explicitly target x64 in UI project

--- Arbeitszeiterfassung.UI/Forms/MainForm.cs
+++ Arbeitszeiterfassung.UI/Forms/MainForm.cs
@@ -1,26 +1,44 @@
-/*
-Titel: MainForm.cs
-Version: 1.0
-Letzte Aktualisierung: 02.06.2025
-Autor: Tanja Trella
-Status: In Bearbeitung
-Datei: /Arbeitszeiterfassung.UI/Forms/MainForm.cs
-Beschreibung: Hauptfenster der Anwendung, Grundlayout mit Logo und Farbschema
-*/
+---
+title: MainForm.cs
+version: 1.1
+lastUpdated: 08.07.2025
+author: Tanja Trella
+status: In Bearbeitung
+file: /Arbeitszeiterfassung.UI/Forms/MainForm.cs
+description: Hauptfenster der Anwendung mit Navigation und Statusleiste
+---
 
+using System;
 using System.Windows.Forms;
-using System.Drawing;
+using Arbeitszeiterfassung.UI.Controls;
+using Arbeitszeiterfassung.UI.Helpers;
 
-namespace Arbeitszeiterfassung.UI.Forms
+namespace Arbeitszeiterfassung.UI.Forms;
+
+/// <summary>
+/// Hauptfenster der Anwendung. Stellt Navigation und Statusleiste bereit.
+/// </summary>
+public partial class MainForm : Form
 {
-    /// <summary>
-    /// Hauptfenster der Anwendung. Stellt Basisnavigation, Farbgebung und Logo bereit.
-    /// </summary>
-    public partial class MainForm : Form
+    /// <summary>Initialisiert eine neue Instanz der <see cref="MainForm"/>.</summary>
+    public MainForm()
     {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        Load += MainForm_Load;
+        sessionTimer.Tick += SessionTimer_Tick;
+    }
+
+    private void MainForm_Load(object? sender, EventArgs e)
+    {
+        UIHelper.ApplyColorScheme(this);
+        statusBarControl.SetUser(Environment.UserName, "Gast");
+        Text = $"Arbeitszeiterfassung - {Environment.UserName}";
+        NavigationHelper.ShowControl(new StartPageControl(), contentPanel);
+        sessionTimer.Start();
+    }
+
+    private void SessionTimer_Tick(object? sender, EventArgs e)
+    {
+        statusBarControl.UpdateTime(DateTime.Now);
     }
 }
